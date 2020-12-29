73e9aefbfc93616def82dbef1b3b2b3500895f6e
Timotheeee <timothe_laborie@yahoo.fr>
2020-12-29 18:01:14 +0100

    cursed btd

---
+++ cursed_btd/Main.cs
@@ -0,0 +1,77 @@
+﻿using MelonLoader;
+using Harmony;
+using NKHook6.Api;
+using Assets.Scripts.Unity.UI_New.InGame.Races;
+using Assets.Scripts.Simulation.Towers.Weapons;
+using NKHook6;
+using Assets.Scripts.Simulation;
+using Assets.Scripts.Unity.UI_New.InGame;
+using NKHook6.Api.Extensions;
+using Assets.Scripts.Unity.UI_New.Main;
+using NKHook6.Api.Events;
+using Assets.Scripts.Simulation.Bloons;
+using Assets.Scripts.Models.Towers;
+
+using Assets.Scripts.Unity;
+
+
+
+using static NKHook6.Api.Events._Towers.TowerEvents;
+using Assets.Scripts.Simulation.Towers;
+
+using static NKHook6.Api.Events._Weapons.WeaponEvents;
+using Assets.Scripts.Utils;
+
+using static NKHook6.Api.Events._TimeManager.TimeManagerEvents;
+using Il2CppSystem.Collections;
+using NKHook6.Api.Events._Bloons;
+using NKHook6.Api.Events._Weapons;
+using Assets.Scripts.Unity.UI_New.Popups;
+using Assets.Scripts.Unity.Bridge;
+using Assets.Scripts.Models.Towers.Behaviors;
+using Assets.Scripts.Simulation.Objects;
+using Assets.Scripts.Models;
+using TMPro;
+using Assets.Scripts.Models.Towers.Behaviors.Attack;
+using UnityEngine;
+using System.Linq;
+
+namespace cursed_btd
+{
+    public class Main : MelonMod
+    {
+
+        public static System.Random r = new System.Random();
+        public static float amount = 0.2f;
+        public static string[] filter = new string[]
+        {
+            "terrain",
+            "camera",
+        };
+
+        public override void OnApplicationStart()
+        {
+            base.OnApplicationStart();
+            EventRegistry.instance.listen(typeof(Main));
+            System.Console.WriteLine("cursed_btd loaded");
+        }
+
+        public override void OnUpdate()
+        {
+            base.OnUpdate();
+            bool inAGame = InGame.instance != null && InGame.instance.bridge != null;
+            foreach (var ob in UnityEngine.Object.FindObjectsOfType<GameObject>())
+            {
+                if (filter.Any(ob.name.ToLower().Contains))
+                    continue;
+                ob.transform.position += new Vector3(r.NextDouble() > 0.5f ? amount : -amount, r.NextDouble() > 0.5f ? amount : -amount, r.NextDouble() > 0.5f ? amount : -amount);
+            }
+        }
+
+
+
+
+
+    }
+
+}
---
+++ cursed_btd/README.md
@@ -0,0 +1,1 @@
+press F8 to use
