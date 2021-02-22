ccab7c3c8ed70261d88e84fa57b6ea692b30017f
Timotheeee <timothe_laborie@yahoo.fr>
2021-02-22 08:35:30 +0100

    shinobi everything

---
+++ shinobi_everything/Main.cs
@@ -0,0 +1,92 @@
+﻿using MelonLoader;
+using Harmony;
+
+using Assets.Scripts.Unity.UI_New.InGame;
+
+using Assets.Scripts.Models.Towers;
+using Assets.Scripts.Unity;
+using Assets.Scripts.Utils;
+using System;
+using System.Text.RegularExpressions;
+using System.IO;
+using Assets.Main.Scenes;
+using UnityEngine;
+using System.Linq;
+using Assets.Scripts.Models.Towers.Behaviors.Attack;
+using Assets.Scripts.Models.Towers.Behaviors.Attack.Behaviors;
+using BloonsTD6_Mod_Helper.Extensions;
+using Assets.Scripts.Models.Towers.Behaviors;
+using Assets.Scripts.Models.Bloons.Behaviors;
+using Assets.Scripts.Models.Towers.Projectiles.Behaviors;
+using System.Collections.Generic;
+using Assets.Scripts.Models;
+using Assets.Scripts.Models.Towers.Projectiles;
+using Assets.Scripts.Models.Towers.Behaviors.Emissions;
+using Assets.Scripts.Models.Towers.Behaviors.Abilities;
+using Assets.Scripts.Simulation.Track;
+
+namespace shinobi_everything
+{
+    public class Main : MelonMod
+    {
+
+
+
+        public override void OnApplicationStart()
+        {
+            base.OnApplicationStart();
+            //EventRegistry.instance.listen(typeof(Main));
+            Console.WriteLine("shinobi_everything mod loaded");
+        }
+
+
+        [HarmonyPatch(typeof(TitleScreen), "Start")]
+        public class Awake_Patch
+        {
+            [HarmonyPostfix]
+            public static void Postfix()
+            {
+                var models = Game.instance.model.towers;
+
+
+                for (int i = 0; i < models.Count; i++)
+                {
+                    var tower = models[i];
+
+                    if (Regex.IsMatch(tower.name, "NinjaMonkey-.3.") || Regex.IsMatch(tower.name, "NinjaMonkey-.4.") || Regex.IsMatch(tower.name, "NinjaMonkey-.5."))
+                    {
+                        tower.GetBehavior<SupportShinobiTacticsModel>().filters = new UnhollowerBaseLib.Il2CppReferenceArray<Assets.Scripts.Models.Towers.TowerFilters.TowerFilterModel>(0);
+                        
+
+
+                    }
+
+
+                }
+
+            }
+        }
+
+
+
+        public override void OnUpdate()
+        {
+            base.OnUpdate();
+
+            bool inAGame = InGame.instance != null && InGame.instance.bridge != null;
+            if (inAGame)
+            {
+
+            }
+        }
+
+
+
+
+
+
+
+
+    }
+
+}
