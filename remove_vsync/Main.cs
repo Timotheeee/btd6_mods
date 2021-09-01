8cfffe2c83bcfbbbdec54afc124c12722c3ecda4
Timotheeee <timothelaborie@gmail.com>
2021-09-01 14:14:34 +0200

    vsync mod

---
+++ remove_vsync/Main.cs
@@ -0,0 +1,68 @@
+﻿using MelonLoader;
+using Harmony;
+
+using Assets.Scripts.Unity.UI_New.InGame.Races;
+using Assets.Scripts.Simulation.Towers.Weapons;
+
+using Assets.Scripts.Simulation;
+using Assets.Scripts.Unity.UI_New.InGame;
+
+using Assets.Scripts.Unity.UI_New.Main;
+
+using Assets.Scripts.Simulation.Bloons;
+using Assets.Scripts.Models.Towers;
+
+using Assets.Scripts.Unity;
+
+
+
+using Assets.Scripts.Simulation.Towers;
+
+
+using Assets.Scripts.Utils;
+
+
+using Il2CppSystem.Collections;
+
+using Assets.Scripts.Unity.UI_New.Popups;
+using Assets.Scripts.Unity.Bridge;
+using Assets.Scripts.Models.Towers.Behaviors;
+using Assets.Scripts.Simulation.Objects;
+using Assets.Scripts.Models;
+using TMPro;
+using Assets.Scripts.Models.Towers.Behaviors.Attack;
+using System;
+using Assets.Scripts.Simulation.Towers.Behaviors;
+using UnityEngine;
+using Assets.Scripts.Unity.Display;
+using System.Linq;
+using System.IO;
+using Assets.Scripts.Models.Towers.Projectiles.Behaviors;
+using BTD_Mod_Helper.Extensions;
+using Assets.Scripts.Unity.Player;
+using Assets.Scripts.Unity.UI_New.Main.MapSelect;
+using BTD_Mod_Helper;
+
+namespace remove_vsync
+{
+    public class Main : BloonsTD6Mod
+    {
+
+
+
+        public override void OnApplicationStart()
+        {
+            base.OnApplicationStart();
+            Console.WriteLine("remove_vsync loaded");
+        }
+
+        public override void OnTitleScreen()
+        {
+            base.OnTitleScreen();
+            QualitySettings.vSyncCount = 0;
+            Application.targetFrameRate = 160;
+        }
+
+    }
+
+}
