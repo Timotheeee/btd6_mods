24e8bd8bdf47435db520b189c231e2f3ff22618f
Timotheeee <timotheeee1@gmail.com>
2022-08-09 22:23:33 +0200

    Update ModHelperData.cs

--- balanced_random_towers/ModHelperData.cs
+++ balanced_random_towers/ModHelperData.cs
@@ -13,7 +13,7 @@ namespace balanced_random_towers
 
         public const string Description = "Randomizes every tower you place to another one with a similar cost";
 
-        public const string RepoOwner = "Timotheeee1";
+        public const string RepoOwner = "Timotheeee";
         public const string RepoName = "btd6_mods";
 
         public const string SubPath = "balanced_random_towers";
