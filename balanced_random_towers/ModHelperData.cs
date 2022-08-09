2cd2d3c81aa330858bdc06d00766cc635bf84874
Timotheeee <timotheeee1@gmail.com>
2022-08-09 21:27:39 +0200

    balanced random to mod helper browser

---
+++ balanced_random_towers/ModHelperData.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace balanced_random_towers
+{
+    public static class ModHelperData
+    {
+        public const string Version = "1.0.0";
+        public const string Name = "Balanced Random Towers";
+
+        public const string Description = "Randomizes every tower you place to another one with a similar cost";
+
+        public const string RepoOwner = "Timotheeee1";
+        public const string RepoName = "btd6_mods";
+
+        public const string SubPath = "balanced_random_towers";
+        public const string DllName = "balanced_random_towers.dll";
+    }
+}
