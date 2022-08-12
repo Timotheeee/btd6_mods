851715230b3b61987c9e5894c5eea457ab9a79f0
Timotheeee <timotheeee1@gmail.com>
2022-08-12 17:36:49 +0200

    Update ModHelperData.cs

--- custom_maps_v2/ModHelperData.cs
+++ custom_maps_v2/ModHelperData.cs
@@ -8,7 +8,7 @@ namespace balanced_random_towers
 {
     public static class ModHelperData
     {
-        public const string Version = "1.0.0";
+        public const string Version = "1.0.3";
         public const string Name = "Custom Maps";
 
         public const string Description = "Adds a lot of new custom maps";
