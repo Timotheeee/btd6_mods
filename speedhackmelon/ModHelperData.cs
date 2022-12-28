2afb7d8e2834e2e02d80d190e35df40f0f868737
Timotheeee <Timotheeee1@gmail.com>
2022-12-28 20:55:29 +0100

    fixed speedhack

---
+++ speedhackmelon/ModHelperData.cs
@@ -0,0 +1,12 @@
+﻿namespace speedhackmelon;
+
+public static class ModHelperData
+{
+    public const string Version = "1.0.1";
+    public const string Name = "speedhackmelon";
+
+    public const string Description = "press keys 6 to 9 to change speed, and F5 to set a custom speed";
+
+    public const string RepoOwner = "Timotheeee";
+    public const string RepoName = "btd6_mods";
+}
