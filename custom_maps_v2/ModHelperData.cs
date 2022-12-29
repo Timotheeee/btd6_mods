6798bbdb8358315d0f6f5a03d938853d0fab3cd4
Timotheeee <Timotheeee1@gmail.com>
2022-12-29 11:00:18 +0100

    size limit

--- custom_maps_v2/ModHelperData.cs
+++ custom_maps_v2/ModHelperData.cs
@@ -5,7 +5,7 @@ public static class ModHelperData
     public const string Version = "1.0.4";
     public const string Name = "custom_maps_v2";
 
-    public const string Description = "Adds a lot of new custom maps.";
+    public const string Description = "Adds a lot of new custom maps. Since the file is above 50 MB, you will have to download the dll directly from github";
 
     public const string RepoOwner = "Timotheeee";
     public const string RepoName = "btd6_mods";
