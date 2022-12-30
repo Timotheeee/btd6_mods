94861ed4a26838bb139930b2630e3e2cee3fd89d
Timotheeee <Timotheeee1@gmail.com>
2022-12-30 10:19:57 +0100

    fix

--- ace_heli_controls/ModHelperData.cs
+++ ace_heli_controls/ModHelperData.cs
@@ -5,7 +5,10 @@ public static class ModHelperData
     public const string Version = "1.0.1";
     public const string Name = "ace_heli_controls";
 
-    public const string Description = "makes the plane controllable like a heli";public const string SubPath = "ace_heli_controls"; public const string DllName = "ace_heli_controls.dll"; public const string WorksOnVersion = "34";
+    public const string Description = "makes the plane controllable like a heli";
+ public const string SubPath = "ace_heli_controls";
+ public const string DllName = "ace_heli_controls.dll";
+ public const string WorksOnVersion = "34";
 
     public const string RepoOwner = "Timotheeee";
     public const string RepoName = "btd6_mods";
