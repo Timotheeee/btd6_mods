9832e3dd8850354e4550612f64ed955cb4df8075
Timotheeee <Timotheeee1@gmail.com>
2022-12-30 10:21:39 +0100

    ace paragon in browser

--- ace_paragon/ModHelperData.cs
+++ ace_paragon/ModHelperData.cs
@@ -5,7 +5,10 @@ public static class ModHelperData
     public const string Version = "1.0.1";
     public const string Name = "ace_paragon";
 
-    public const string Description = "";
+    public const string Description = "adds ace paragon";
+ public const string SubPath = "ace_paragon";
+ public const string DllName = "ace_paragon.dll";
+ public const string WorksOnVersion = "34";
 
     public const string RepoOwner = "Timotheeee";
     public const string RepoName = "btd6_mods";
