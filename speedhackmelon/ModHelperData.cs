8c9e5220aba3014f4a0069f7cfb4028358f7840e
Timotheeee <Timotheeee1@gmail.com>
2023-02-16 09:35:58 +0100

    version

--- speedhackmelon/ModHelperData.cs
+++ speedhackmelon/ModHelperData.cs
@@ -2,7 +2,7 @@ namespace speedhackmelon;
 
 public static class ModHelperData
 {
-    public const string Version = "1.0.1";
+    public const string Version = "1.0.2";
     public const string Name = "speedhackmelon";
 
     public const string Description = "";
