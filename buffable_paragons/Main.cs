999367ff86fda3005f7d795488fe1742e0f3f07b
Timotheeee <Timotheeee1@gmail.com>
2023-06-08 20:41:41 +0200

    code cleanup

--- buffable_paragons/Main.cs
+++ buffable_paragons/Main.cs
@@ -26,12 +26,13 @@ using Il2CppAssets.Scripts.Models.Towers.Behaviors.Abilities;
 using Il2CppAssets.Scripts.Simulation.Track;
 using BTD_Mod_Helper.Api.Towers;
 using BTD_Mod_Helper;
+using static Il2CppAssets.Scripts.Models.Towers.Behaviors.ParagonTowerModel;
 
-[assembly: MelonInfo(typeof(buffable_paragons.Main), buffable_paragons.ModHelperData.Name, buffable_paragons.ModHelperData.Version, buffable_paragons.ModHelperData.RepoOwner)]
+[assembly: MelonInfo(typeof(buffable_paragons.Main0), buffable_paragons.ModHelperData.Name, buffable_paragons.ModHelperData.Version, buffable_paragons.ModHelperData.RepoOwner)]
 [assembly: MelonGame("Ninja Kiwi", "BloonsTD6")]
 namespace buffable_paragons
 {
-    public class Main : BloonsTD6Mod
+    public class Main0 : BloonsTD6Mod
     {
 
 
@@ -42,8 +43,48 @@ namespace buffable_paragons
             Console.WriteLine("buffable_paragons loaded");
         }
 
+        public static void Process(TowerModel towerModel, string name)
+        {
+            towerModel.isParagon = false;
+            towerModel.isBakable = true;
+            towerModel.RemoveBehavior<ParagonTowerModel>();
 
+            var b = Game.instance.model.GetTowerFromId(name);
+            towerModel.icon = new SpriteReference() { guidRef = b.portrait.guidRef };
+            towerModel.instaIcon = new SpriteReference() { guidRef = b.portrait.guidRef };
+            towerModel.portrait = new SpriteReference() { guidRef = b.portrait.guidRef };
+        }
 
+        //public override void OnUpdate()
+        //{
+        //    base.OnUpdate();
+
+        //    bool inAGame = InGame.instance != null && InGame.instance.bridge != null;
+        //    if (inAGame)
+        //    {
+        //        //var buffmodel = InGame.instance.GetGameModel().paragonDegreeDataModel;
+        //        //buffmodel.
+        //        foreach (var tower in InGame.instance.bridge.GetAllTowers())
+        //        {
+        //            if (tower.tower.model.name.Contains("Paragon"))
+        //            {
+        //                Console.WriteLine(tower.tower.model.name);
+        //                //tower.tower.AddParagonMutator(new PowerDegreeMutator(100,0.5f))
+        //                foreach(var mut in tower.tower.mutators)
+        //                {
+        //                    Console.WriteLine(mut.ToString());
+        //                }
+        //            }
+        //        }
+
+        //    }
+
+        //}
+
+    }
+
+    public class Main1 : BloonsTD6Mod
+    {
         public class DartParagon : ModTower
         {
             public override string BaseTower => "DartMonkey-Paragon";
@@ -55,26 +96,12 @@ namespace buffable_paragons
             public override int BottomPathUpgrades => 0;
             public override string DisplayName => "DartMonkey Paragon Buffable";
             public override string Description => "Buffable";
-
             public override string Icon => "3906afef12a8fab4db780428c3b8a50b";
-
             public override void ModifyBaseTowerModel(TowerModel towerModel)
             {
-                towerModel.isParagon = false;
-                towerModel.isBakable = true;
-                towerModel.RemoveBehavior<ParagonTowerModel>();
-
-                var b = Game.instance.model.GetTowerFromId("DartMonkey-Paragon");
-                towerModel.icon = new SpriteReference() { guidRef = b.portrait.guidRef };
-                towerModel.instaIcon = new SpriteReference() { guidRef = b.portrait.guidRef };
-                towerModel.portrait = new SpriteReference() { guidRef = b.portrait.guidRef };
-
+                Main0.Process(towerModel, "DartMonkey-Paragon");
             }
         }
-
-
-
-
     }
 
     public class Main2 : BloonsTD6Mod
@@ -90,23 +117,12 @@ namespace buffable_paragons
             public override int BottomPathUpgrades => 0;
             public override string DisplayName => "MonkeyBuccaneer Paragon Buffable";
             public override string Description => "Buffable";
-
-
-
             public override void ModifyBaseTowerModel(TowerModel towerModel)
             {
-                towerModel.isParagon = false;
-                towerModel.isBakable = true;
-                towerModel.RemoveBehavior<ParagonTowerModel>();
-
-                var b = Game.instance.model.GetTowerFromId("MonkeyBuccaneer-Paragon");
-                towerModel.icon = new SpriteReference() { guidRef = b.portrait.guidRef };
-                towerModel.instaIcon = new SpriteReference() { guidRef = b.portrait.guidRef };
-                towerModel.portrait = new SpriteReference() { guidRef = b.portrait.guidRef };
+                Main0.Process(towerModel, "MonkeyBuccaneer-Paragon");
             }
         }
     }
-
 
     public class Main3 : BloonsTD6Mod
     {
@@ -121,22 +137,12 @@ namespace buffable_paragons
             public override int BottomPathUpgrades => 0;
             public override string DisplayName => "BoomerangMonkey Paragon Buffable";
             public override string Description => "Buffable";
-
-
             public override void ModifyBaseTowerModel(TowerModel towerModel)
             {
-                towerModel.isParagon = false;
-                towerModel.isBakable = true;
-                towerModel.RemoveBehavior<ParagonTowerModel>();
-
-                var b = Game.instance.model.GetTowerFromId("BoomerangMonkey-Paragon");
-                towerModel.icon = new SpriteReference() { guidRef = b.portrait.guidRef };
-                towerModel.instaIcon = new SpriteReference() { guidRef = b.portrait.guidRef };
-                towerModel.portrait = new SpriteReference() { guidRef = b.portrait.guidRef };
+                Main0.Process(towerModel, "BoomerangMonkey-Paragon");
             }
         }
     }
-
 
     public class Main4 : BloonsTD6Mod
     {
@@ -151,22 +157,13 @@ namespace buffable_paragons
             public override int BottomPathUpgrades => 0;
             public override string DisplayName => "NinjaMonkey Paragon Buffable";
             public override string Description => "Buffable";
-
-
-
             public override void ModifyBaseTowerModel(TowerModel towerModel)
             {
-                towerModel.isParagon = false;
-                towerModel.isBakable = true;
-                towerModel.RemoveBehavior<ParagonTowerModel>();
-
-                var b = Game.instance.model.GetTowerFromId("NinjaMonkey-Paragon");
-                towerModel.icon = new SpriteReference() { guidRef = b.portrait.guidRef };
-                towerModel.instaIcon = new SpriteReference() { guidRef = b.portrait.guidRef };
-                towerModel.portrait = new SpriteReference() { guidRef = b.portrait.guidRef };
+                Main0.Process(towerModel, "NinjaMonkey-Paragon");
             }
         }
     }
+
     public class Main5 : BloonsTD6Mod
     {
         public class MonkeyBuccaneer : ModTower
@@ -180,21 +177,31 @@ namespace buffable_paragons
             public override int BottomPathUpgrades => 0;
             public override string DisplayName => "EngineerMonkey Paragon Buffable";
             public override string Description => "Buffable";
-
-
-
             public override void ModifyBaseTowerModel(TowerModel towerModel)
             {
-                towerModel.isParagon = false;
-                towerModel.isBakable = true;
-                towerModel.RemoveBehavior<ParagonTowerModel>();
-
-                var b = Game.instance.model.GetTowerFromId("EngineerMonkey-Paragon");
-                towerModel.icon = new SpriteReference() { guidRef = b.portrait.guidRef };
-                towerModel.instaIcon = new SpriteReference() { guidRef = b.portrait.guidRef };
-                towerModel.portrait = new SpriteReference() { guidRef = b.portrait.guidRef };
+                Main0.Process(towerModel, "EngineerMonkey-Paragon");
             }
         }
     }
+
+    public class Main6 : BloonsTD6Mod
+    {
+        public class MonkeyBuccaneer : ModTower
+        {
+            public override string BaseTower => "WizardMonkey-Paragon";
+            public override string Name => "WizardMonkey-Paragon-Buffable";
+            public override int Cost => 540000;
+            public override Il2CppAssets.Scripts.Models.TowerSets.TowerSet TowerSet => Il2CppAssets.Scripts.Models.TowerSets.TowerSet.Primary;
+            public override int TopPathUpgrades => 0;
+            public override int MiddlePathUpgrades => 0;
+            public override int BottomPathUpgrades => 0;
+            public override string DisplayName => "WizardMonkey Paragon Buffable";
+            public override string Description => "Buffable";
+            public override void ModifyBaseTowerModel(TowerModel towerModel)
+            {
+                Main0.Process(towerModel, "WizardMonkey-Paragon");
+            }
+        }
+    } 
 
 }
