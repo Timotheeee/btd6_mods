e32108feb766faaca3313bad916995d40d97a3c1
Timotheeee <Timotheeee1@gmail.com>
2023-07-31 14:35:58 +0200

    description

--- all_bosses_at_once/ModHelperData.cs
+++ all_bosses_at_once/ModHelperData.cs
@@ -5,7 +5,7 @@ public static class ModHelperData
     public const string Version = "38.1";
     public const string Name = "all bosses at once";
 
-    public const string Description = "on rounds 40, 60 etc all 4 elite bosses come out at once. On round 120 you will have to manually spawn-in the bosses using leftshift+F7. If you go back to menu, make sure to start playing the round before pressing the hotkey again.";
+    public const string Description = "Let's you fight all elite bosses at once, including Phayze. On rounds 40, 60 etc. press shift+F7 to spawn the bosses. If you go back to menu, make sure to start playing the round before pressing the hotkey again. It is fine if you forget to send the bosses on the corresponding round, pressing the hotkey on a round like 62 will spawn the tier 2 bosses as if it was round 60.";
  public const string SubPath = "all_bosses_at_once";
  public const string DllName = "all_bosses_at_once.dll";
  public const string WorksOnVersion = "34";
