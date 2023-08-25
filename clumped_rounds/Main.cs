3af6608e6c7611a24f261a271ed0bb3fc236cbb2
Timotheeee <Timotheeee1@gmail.com>
2023-08-25 23:09:41 +0200

    clumped rounds fix

--- clumped_rounds/Main.cs
+++ clumped_rounds/Main.cs
@@ -22,12 +22,15 @@ using Il2CppAssets.Scripts.Models.Rounds;
 using Il2CppInterop.Runtime; using Il2CppInterop.Runtime.InteropTypes; using Il2CppInterop.Runtime.InteropTypes.Arrays;
 using System;
 using Il2CppAssets.Scripts.Unity.Scenes;
+using BTD_Mod_Helper.Api.Bloons;
+using BTD_Mod_Helper.Api.Enums;
+using BTD_Mod_Helper;
 
 [assembly: MelonInfo(typeof(clumped_rounds.Main), clumped_rounds.ModHelperData.Name, clumped_rounds.ModHelperData.Version, clumped_rounds.ModHelperData.RepoOwner)]
 [assembly: MelonGame("Ninja Kiwi", "BloonsTD6")]
 namespace clumped_rounds
 {
-    public class Main : MelonMod
+    public class Main : BloonsTD6Mod
     {
 
 
@@ -38,29 +41,46 @@ namespace clumped_rounds
             Console.WriteLine("clumped_rounds loaded");
         }
 
-
-        [HarmonyPatch(typeof(TitleScreen), "Start")]
-        public class Game_Patch
+        public class clumpedRounds : ModRoundSet
         {
-            [HarmonyPostfix]
-            public static void Postfix()
-            {
-                for (int i = 0; i < Game.instance.model.roundSets.Length; i++)
-                {
-                    RoundSetModel roundSet = Game.instance.model.roundSets[i];
-                    for (int j = 0; j < roundSet.rounds.Length; j++)
-                    {
-                        RoundModel round = roundSet.rounds[j];
-                        foreach (var group in round.groups)
-                        {
-                            group.start = 0;
-                            group.end = 0;
-                        }
+            public override string BaseRoundSet => RoundSetType.Default;
+            public override int DefinedRounds => BaseRounds.Count;
+            public override string DisplayName => "Clumped Rounds";
+            public override string Icon => VanillaSprites.LargerPotionsUpgradeIcon;
 
-                    }
+            public override void ModifyRoundModels(RoundModel roundModel, int round)
+            {
+                foreach (var group in roundModel.groups)
+                {
+                    group.start = 0;
+                    group.end = 0;
                 }
             }
         }
+
+
+        //[HarmonyPatch(typeof(TitleScreen), "Start")]
+        //public class Game_Patch
+        //{
+        //    [HarmonyPostfix]
+        //    public static void Postfix()
+        //    {
+        //        for (int i = 0; i < Game.instance.model.roundSets.Length; i++)
+        //        {
+        //            RoundSetModel roundSet = Game.instance.model.roundSets[i];
+        //            for (int j = 0; j < roundSet.rounds.Length; j++)
+        //            {
+        //                RoundModel round = roundSet.rounds[j];
+        //                foreach (var group in round.groups)
+        //                {
+        //                    group.start = 0;
+        //                    group.end = 0;
+        //                }
+
+        //            }
+        //        }
+        //    }
+        //}
 
 
 
