0870f2b631901acb1da52e573d935040b401b897
Timotheeee <Timotheeee1@gmail.com>
2024-05-08 19:42:24 +0200

    custom maps fix

--- custom_maps_v2/ModHelperData.cs
+++ custom_maps_v2/ModHelperData.cs
@@ -2,7 +2,7 @@ namespace custommaps;
 
 public static class ModHelperData
 {
-    public const string Version = "39";
+    public const string Version = "42";
     public const string Name = "custom maps v2";
 
     public const string Description = "Adds a lot of new custom maps. Since the file is above 50 MB, you will have to download the dll directly from github";
