5a18cb3a56c3548cac8578fb5abe269e887cd8f7
Midnight <23142346+Midnight145@users.noreply.github.com>
2026-02-27 03:09:10 -0600

    make unlimited heroes persist on save load after game restart

--- unlimited_heroes/ModHelperData.cs
+++ unlimited_heroes/ModHelperData.cs
@@ -2,13 +2,13 @@ namespace unlimited_heroes;
 
 public static class ModHelperData
 {
-    public const string Version = "37";
+    public const string Version = "38";
     public const string Name = "unlimited heroes";
 
     public const string Description = "Allows you to use every hero at once, made by gurren";
  public const string SubPath = "unlimited_heroes";
  public const string DllName = "unlimited_heroes.dll";
- public const string WorksOnVersion = "34";
+ public const string WorksOnVersion = "53";
 
     public const string RepoOwner = "Timotheeee";
     public const string RepoName = "btd6_mods";
